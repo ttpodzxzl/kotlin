5a818b12d5e58d1c0238b7614c68f4655a5f3658
Alexey Trilis <aatrilis2000@gmail.com>
2019-09-17 16:52:39 +0300

    Remove tests and tests-related files

--- libraries/tools/dukat/build.gradle.kts
+++ libraries/tools/dukat/build.gradle.kts
@@ -10,10 +10,6 @@ dependencies {
     implementation(kotlinStdlib())
     implementation("org.jetbrains.dukat:dukat:0.0.17")
     implementation("org.jsoup:jsoup:1.8.2")
-
-    testCompile(projectTests(":idea"))
-    testCompile(projectTests(":compiler:tests-common"))
-    testCompile(intellijCoreDep()) { includeJars("intellij-core") }
 }
 
 task("downloadIDL", JavaExec::class) {
@@ -26,11 +22,4 @@ task("launchDukat", JavaExec::class) {
     main = "org.jetbrains.kotlin.tools.dukat.LaunchKt"
     classpath = sourceSets["main"].runtimeClasspath
     dependsOn(":dukat:build")
-}
-
-projectTest {
-    doFirst {
-        workingDir = rootDir
-        systemProperty("idea.home.path", intellijRootDir().canonicalPath)
-    }
 }
